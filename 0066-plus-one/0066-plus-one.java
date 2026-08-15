6ba84b1ffd65305a5dd01692e89d4e689e0b64ee
Rohindan <rohindan42rohindan@gmail.com>
2026-08-15 22:40:27 +0530

    Time: 0 ms (100%), Space: 43.3 MB (80.04%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,26 +1,47 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        int[] arr = {1};
+        int[] sum = new int[n];
 
-        // Traverse from the last digit
-        for (int i = digits.length - 1; i >= 0; i--) {
+        int i = digits.length-1;
+        int j = arr.length-1;
+        int k = sum.length-1;
 
-            // If the current digit is less than 9,
-            // simply add 1 and return.
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
+        int c = 0;
+        while(k >= 0){
+            int d = c;
+
+            if( i >= 0){
+                d += digits[i];
+            }
+            if(j >= 0){
+                d += arr[j];
             }
 
-            // If the current digit is 9,
-            // make it 0 and carry 1 to the previous digit.
-            digits[i] = 0;
+            c = d / 10;
+            d = d % 10;
+
+            sum[k] = d;
+
+            i--;
+            j--;
+            k--;
+
+
+
         }
+        if(c > 0){
+            int[] res = new int[n+1];
+            res[0] = c;
+            for(int x = 0; x < n; x++){
+                res[x+1] = sum[x];
+            }
+            return res;
+        }
+        
+        return sum;
 
-        // If all digits were 9,
-        // create a new array with one extra digit.
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
 
-        return result;
     }
 }
