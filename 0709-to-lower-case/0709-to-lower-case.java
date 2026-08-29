cad9c3799745b975bf933b08055752920dc361e4
Rohindan <rohindan42rohindan@gmail.com>
2026-08-29 20:22:29 +0530

    Time: 0 ms (100%), Space: 42.7 MB (92.54%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,13 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        for(int i = 0; i < s.length(); i++){
-            char ch = sb.charAt(i);
-            if(ch >= 'A' && ch <= 'Z'){
-                char lch = (char) ('a' + ch - 'A');
-                sb.setCharAt(i,lch);
-            }
-        }
-        return sb.toString();
+        return s.toLowerCase();
     }
 }
