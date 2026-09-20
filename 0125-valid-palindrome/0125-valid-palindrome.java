c51c5ca1f45fd60863d75a2a659436d93b9eb7ec
Rohindan <rohindan42rohindan@gmail.com>
2026-09-21 00:30:27 +0530

    Time: 3 ms (56.02%), Space: 44.9 MB (38.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isPalindrome(String s) {
 
         StringBuilder ss = new StringBuilder();
+        
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
             if(Character.isLetterOrDigit(ch)){
@@ -12,7 +13,7 @@ class Solution {
 
         int i = 0;
         int j = ss.length()-1;
-        while(i <= j){
+        while(i < j){
             char ch1 = ss.charAt(i);
             char ch2 = ss.charAt(j);
             if(ch1 != ch2){
